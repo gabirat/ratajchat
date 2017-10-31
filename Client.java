2e9a8be9c5e15680ae8d11f8869d366338458a93
gabirat <gabirat@onet.pl>
2017-10-31 11:39:37 +0100

    Added sending message objecdts and listener on close NOT TESTED YET

--- Client.java
+++ Client.java
@@ -10,26 +10,51 @@ public class Client {
         int port = Integer.parseInt(args[1]);
 
         EventQueue.invokeLater(new Runnable() {
-            ChatWindow chatWindow;
+            ChatWindow chatWindow = null;
+            Socket client = null;
+            ObjectOutputStream out = null;
+            ObjectInputStream in = null;
 
             @Override
             public void run() {
                 chatWindow = new ChatWindow();
 
+                chatWindow.send.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        ChatMessage msg = new ChatMessage();
+                        String messageText = chatWindow.userInput.getText();
+                        msg.author = "Unnknown";
+                        msg.message = messageText;
+                        if (!client.isConnected()) {
+                            chatWindow.messages.append("Wait for the chat to connect to the server! \n");
+                        } else {
+                            out.writeObject(msg);
+                        }
+                    }
+                });
+
+                chatWindow.addWindowListener(new WindowAdapter() { //FIXME
+                    @Override
+                    public void windowClosing(WindowEvent e) {
+                        System.out.println("Closed");
+                        client.close();
+                    }
+                });
+
                 try {
                     chatWindow.messages.append("Connecting to " + serverName + " on port " + port + "\n");
-                    Socket client = new Socket(serverName, port);
+                    client = new Socket(serverName, port);
 
                     chatWindow.messages.append("Just connected to " + client.getRemoteSocketAddress() + "\n");
-                    OutputStream outToServer = client.getOutputStream();
-                    DataOutputStream out = new DataOutputStream(outToServer);
+                    //OutputStream outToServer = client.getOutputStream();
+                    out = new ObjectOutputStream(client.getOutputStream());
 
                     out.writeUTF("Hello from " + client.getLocalSocketAddress());
-                    InputStream inFromServer = client.getInputStream();
-                    DataInputStream in = new DataInputStream(inFromServer);
+                    //InputStream inFromServer = client.getInputStream();
+                    in = new ObjectInputStream(client.getInputStream());
 
                     chatWindow.messages.append("Server says " + in.readUTF() + "\n");
-                    client.close();
+                    
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
