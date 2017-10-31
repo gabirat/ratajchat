2c2c48898fa8640d39240c66c07d8116310db089
gabirat <gabirat@onet.pl>
2017-10-31 11:04:28 +0100

    Added new layout

--- ChatWindow.java
+++ ChatWindow.java
@@ -5,7 +5,7 @@ public class ChatWindow extends JFrame{
     public JTextArea messages;
     public JButton send;
     public JTextField userInput;
-    public JMenuBar menuBar;
+    JMenuBar menuBar;
     JMenu options;
     FlowLayout layout;
 
@@ -13,18 +13,24 @@ public class ChatWindow extends JFrame{
         super("RatajChat v1.0");
         setSize(400, 400);
         messages = new JTextArea();
+        messages.setColumns(32);
+        messages.setRows(18);
+        messages.setEditable(false);
+        messages.setLineWrap(true);
         send = new JButton("Wyslij");
         userInput = new JTextField();
+        userInput.setColumns(25);
         menuBar = new JMenuBar();
         options = new JMenu("Opcje");
         layout = new FlowLayout();
         menuBar.add(options);
         setLayout(layout);
+        setJMenuBar(menuBar);
         add(messages);
-        add(send);
         add(userInput);
-        add(menuBar);
+        add(send);
         setVisible(true);
+        setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 }
